6eafeb1d23af27efdf6632ff979b4b42abe0ec04
Jonatan Snyders <jonatansnyders@gmail.com>
2024-07-12 17:45:51 +0200

    passing categories, materials etc from view to template

--- a_products/views.py
+++ a_products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category, Purpose, Material, BodyPart
 import logging
 
 logger = logging.getLogger(__name__)
@@ -7,5 +7,16 @@ logger = logging.getLogger(__name__)
 def view_list(request):
     # use custom 'filter_by_params' to filter the products on request.GET search parameters
     products = Product.objects.filter_by_params(**request.GET)
+    print('Products count: ', products.count())
+
+    context = {
+        'products': products,
+        'categories': Category.objects.all(),
+        'purposes': Purpose.objects.all(),
+        'materials': Material.objects.all(),
+        'body_parts': BodyPart.objects.all(),
+    }
     
-    return render(request, 'products/product_list.html', {'products': products})
+    # if request.htmx:
+    #     return render(request, 'products/product_list.html#product_list', context)
+    return render(request, 'products/product_list.html', context)
