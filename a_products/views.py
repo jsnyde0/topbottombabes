b2f67319ec56c81dd6386f7e87b4d853f9fa602e
Jonatan Snyders <jonatansnyders@gmail.com>
2024-07-16 18:21:56 +0200

    fixed cart element in header navbar

--- a_products/views.py
+++ a_products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product, Category, Purpose, Material, BodyPart
+from a_cart.models import Cart
 import logging
 import time
 
@@ -8,8 +9,9 @@ logger = logging.getLogger(__name__)
 def view_list(request):
     # use custom 'filter_by_params' to filter the products on request.GET search parameters
     products = Product.objects.filter_by_params(**request.GET)
+    cart, created = Cart.objects.get_or_create(user=request.user)  
 
-    context = {'products': products}
+    context = {'products': products, 'cart': cart}
     
     time.sleep(1)
 
