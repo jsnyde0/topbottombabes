a9d5696be09cf21581fb5b9dfa7c6b440557bd16
Jonatan Snyders <jonatansnyders@gmail.com>
2024-07-16 17:47:44 +0200

    fixed serving media files in development

--- a_core/urls.py
+++ a_core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('thebaws/', admin.site.urls),
@@ -23,3 +25,5 @@ urlpatterns = [
     path('', include('a_products.urls')),
     path('cart/', include('a_cart.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
