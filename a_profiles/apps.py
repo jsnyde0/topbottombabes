d1d526e8c55e2e3f8709b8a8a74b2d356a50fdbf
Jonatan Snyders <jonatansnyders@gmail.com>
2024-07-17 19:12:18 +0200

    updated a_profiles app config so that signals are loaded

--- a_profiles/apps.py
+++ a_profiles/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'a_profiles'
+
+    def ready(self):
+        import a_profiles.signals #noqa
